95ee4e8cea9da295ea154caf12a624a9060a2aa9
CHATELET-Arnaud <arnaud.chatelet@centraliens-marseille.fr>
2020-06-25 23:55:10 +0200

    Add project files.

---
+++ TDD Unit Test 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TDD_Unit_Test_1
+{
+    class Program
+    {
+        /*static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            int resultat = Addition(a, b);
+
+            if (resultat != 3)
+                Console.WriteLine("Le test a raté");
+            else
+                Console.WriteLine("Le test est OK");
+        }
+        
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }*/
+        static void Main(string[] args)
+        {
+            string phrase1 = "";
+            if (CompteMots(phrase1) != 0)
+                Console.WriteLine("Echec du test");
+        }
+
+        public static int CompteMots(string phrase)
+        {
+            return 0;
+        }
+    }
+}
